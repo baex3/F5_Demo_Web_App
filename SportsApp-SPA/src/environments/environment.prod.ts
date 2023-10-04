780e16613addd506cf257cf57b657fde309ee029
baex3 <baex@aggienetwork.com>
2023-10-04 15:04:00 -0500

    Revert "Merge https://github.com/baex3/F5_Demo_Web_App"
    This reverts commit c4526f0bffdfffaea1cf84f228e1ab3191074f19, reversing
    changes made to 74dbdc6c05207a9951dbfe3a9a764fac9a6efa49.

--- SportsApp-SPA/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true
-  urlAddress: 'http://192.168.3.7:5000'
-};
--- SportsApp.API/DTOs/UserForLoginDto.cs
+++
@@ -1,9 +0,0 @@
-namespace SportsApp.API.Controllers
-{
-    public class UserForLoginDto
-    {
-        public string Username {get; set;}
-
-        public string Password {get; set;}
-    }
-}
